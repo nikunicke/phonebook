859fd34a0bb61213f700ce5716851a0cd259c771
nikunicke <pimenoff.niklas@gmail.com>
2019-04-05 17:22:54 +0300

    fixed PORT

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ app.delete('/api/persons/:id', (req, res) => {
 
 
 
-const port = 3001
+const port = process.env.PORT || 3001
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
