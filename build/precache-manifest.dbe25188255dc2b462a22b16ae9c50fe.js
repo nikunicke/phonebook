84e931f88b5ecac9f921641dc836e74ec82efea5
nikunicke <pimenoff.niklas@gmail.com>
2019-04-16 14:53:25 +0300

    folder fix

---
+++ build/precache-manifest.dbe25188255dc2b462a22b16ae9c50fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a94758770939001cb22c",
+    "url": "/static/js/main.7fc4ad77.chunk.js"
+  },
+  {
+    "revision": "fd5a606f7d58ec4b1f29",
+    "url": "/static/js/2.1bb1729d.chunk.js"
+  },
+  {
+    "revision": "a94758770939001cb22c",
+    "url": "/static/css/main.975c5ea5.chunk.css"
+  },
+  {
+    "revision": "3b4f306834d87eea3d8d4f4de871ed8e",
+    "url": "/index.html"
+  }
+];
